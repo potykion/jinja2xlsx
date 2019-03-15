0fd34a08fd9b24ff48fd63907d4b217a93ff6af8
potykion <potykion@gmail.com>
2019-03-16 01:11:39 +0300

    Colgroup assertion

--- jinja2xlsx/api.py
+++ jinja2xlsx/api.py
@@ -1,4 +1,5 @@
 from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 from requests_html import HTML
 
 
@@ -7,16 +8,17 @@ def render(html_str: str) -> Workbook:
 
     table = html.find("table", first=True)
 
-    # columns = table.find("colgroup", first=True).find("col")
-    # assert columns, "No colgroup with col defined"
+    columns = table.find("colgroup", first=True).find("col")
+    assert columns, "No colgroup with col defined"
 
     table_rows = table.find("tbody", first=True).find("tr")
-    row_values = ((td.text for td in row.find("td")) for row in table_rows)
+    row_values = ([td.text for td in row.find("td")] for row in table_rows)
 
     wb = Workbook()
-    ws = wb.active
+    ws: Worksheet = wb.active
 
-    for row in row_values:
-        ws.append(list(row))
+    for row_index, row in enumerate(row_values):
+        for col_index, col in enumerate(columns):
+            ws.cell(row_index + 1, col_index + 1).value = row[col_index]
 
     return wb
--- tests/test_table_creation.py
+++ tests/test_table_creation.py
@@ -1,12 +1,44 @@
 import os
+from contextlib import contextmanager
+from typing import IO, List, Tuple, Iterable, Iterator, TextIO
+
+import pytest
+from openpyxl import load_workbook, Workbook
 
 from jinja2xlsx.api import render
 from jinja2xlsx.config import TEST_DATA_DIR
 
 
+def get_test_file_path(file_: str) -> str:
+    return os.path.join(TEST_DATA_DIR, file_)
+
+
+@contextmanager
+def read_from_test_dir(file_: str) -> Iterator[TextIO]:
+    with open(get_test_file_path(file_)) as f:
+        yield f
+
+
+def get_wb_values(wb: Workbook) -> List[Tuple]:
+    return list(wb.active.values)
+
+
 def test_xlsx_table_creation_from_html_table() -> None:
-    with open(os.path.join(TEST_DATA_DIR, "table.html")) as f:
+    with read_from_test_dir("table.html") as f:
         html_table = f.read()
 
     wb = render(html_table)
-    assert list(wb.active.values) == [("1", "2"), ("3", "4")]
+    assert get_wb_values(wb) == [("1", "2"), ("3", "4")]
+
+
+@pytest.mark.skip("WIP")
+def test_xlsx_table_creation_from_html_table_with_merged_cells() -> None:
+    with read_from_test_dir("table_with_merged_cells.html") as f:
+        html_table = f.read()
+        actual_wb = render(html_table)
+        actual_wb_values = get_wb_values(actual_wb)
+
+    expected_wb = load_workbook(get_test_file_path("table_with_merged_cells.html.xlsx"))
+    expected_wb_values = get_wb_values(expected_wb)
+
+    assert actual_wb_values == expected_wb_values
